b33ef696c488ef6ca765f18221802a99de2e0098
Mostafa Khaled <mostafa.khaled.5422@gmail.com>
2023-02-15 17:34:21 +0200

    add illegal tests

--- src/bin/decafcc/parser/test.rs
+++ src/bin/decafcc/parser/test.rs
@@ -41,4 +41,32 @@ mod legal {
     test_legal!(legal_25, 25);
     test_legal!(legal_26, 26);
     test_legal!(legal_27, 27);
+
+    test_illegal!(illegal_01, 01);
+    test_illegal!(illegal_02, 02);
+    test_illegal!(illegal_03, 03);
+    test_illegal!(illegal_04, 04);
+    test_illegal!(illegal_05, 05);
+    test_illegal!(illegal_06, 06);
+    test_illegal!(illegal_07, 07);
+    test_illegal!(illegal_08, 08);
+    test_illegal!(illegal_09, 09);
+    test_illegal!(illegal_10, 10);
+    test_illegal!(illegal_11, 11);
+    test_illegal!(illegal_12, 12);
+    test_illegal!(illegal_13, 13);
+    test_illegal!(illegal_14, 14);
+    test_illegal!(illegal_15, 15);
+    test_illegal!(illegal_16, 16);
+    test_illegal!(illegal_17, 17);
+    test_illegal!(illegal_18, 18);
+    test_illegal!(illegal_19, 19);
+    test_illegal!(illegal_20, 20);
+    test_illegal!(illegal_21, 21);
+    test_illegal!(illegal_22, 22);
+    test_illegal!(illegal_23, 23);
+    test_illegal!(illegal_24, 24);
+    test_illegal!(illegal_25, 25);
+    test_illegal!(illegal_26, 26);
+    test_illegal!(illegal_27, 27);
 }
